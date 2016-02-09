db1bcd30b0240bbe445b17fc83e0a5b97f5497e1
guskant <gusni.kantu@gmail.com>
2016-02-09 01:33:40 +0000

    jmina lo peg zei curve datnyvei

---
+++ pegpoho.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DETRI="`date -u +%Y%m%d`"
+awk --assign awk_detri="$DETRI" '
+	BEGIN {
+	print "\057\057 di\047e ve vimcu lo javaskript zei pagbu la\047o zoi https://raw.githubusercontent.com/lojban/ilmentufa/gh-pages/camxes.js.peg zoi de\047i li " awk_detri " tede\047i UTC fa\047o"
+	}
+	/^$/ { next }
+	/^ *\057/ { next }
+	/^\173/,/^\175/ { next }
+	{
+	sub(/=/, "<-");
+	sub(/expr:\(?/, "");
+	sub(/\)? +\173.*$/, "");
+	sub(/pre:/, "");
+	sub(/post:/, "");
+	print }
+	' camxes.js.peg > "camxes-"$DETRI".peg"
+
+
