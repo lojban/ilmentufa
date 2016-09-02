f041b6e3864fdb19050bfd97874f46338fcb8d49
Ilmen <ilmen.pokebip@gmail.com>
2016-09-02 17:12:41 +0200

    camxes.html, camxes_postproc.js: Moving the morphology removal code to the postprocessor script. Removing dependency on process_parse_tree.js, whose content has essentially be moved to the postprocessor.

--- camxes_postproc.js
+++ camxes_postproc.js
@@ -1,7 +1,7 @@
 /*
  * CAMXES.JS POSTPROCESSOR
  * Created by Ilmen (ilmen.pokebip <at> gmail.com) on 2013-08-16.
- * Last change: 2016-03-24.
+ * Last change: 2016-09-02.
  * 
  * Entry point: camxes_postprocessing(input, mode, ptproc, postproc_id)
  * Arguments:
@@ -35,6 +35,11 @@
  *   -- new_postprocessor(input, no_morpho, with_selmaho, with_terminator)
  *   -- prune_unwanted_nodes(tree, is_wanted_node)
  *   -- prefix_wordclass(tree, replacements)
+ *   -- remove_morphology(pt)
+ *   -- is_target_node(n)
+ *   -- join_expr(n)
+ *   -- among(v, s)
+ *   -- is_selmaho(v)
  *   -- old_postprocessor(text, with_selmaho, without_terminator, with_nodes_labels)
  *   -- erase_elided_terminators(str)
  *   -- delete_superfluous_brackets(str)
@@ -59,8 +64,9 @@ if (typeof alert !== 'function')
  * If the new postprocessor proves satisfactory, the older postprocessor's code
  * will be entirely removed.
  */
-function camxes_postprocessing(input, mode, ptproc, postproc_id) {
+function camxes_postprocessing(input, mode, postproc_id) {
     var with_spaces = mode & 8;
+    var without_morphology = mode & 16;
     mode = mode % 8;
     var with_selmaho = (mode != 2 && mode != 5);
     var with_nodes_labels = (mode == 4 || mode == 7);
@@ -69,15 +75,15 @@ function camxes_postprocessing(input, mode, ptproc, postproc_id) {
     // Default: older postprocessor.
     if (typeof postproc_id !== 'number') postproc_id = 1;
     if (is_array(input)) {
-        if (typeof ptproc === 'function')
-            input = ptproc(input); // Deleting morphology nodes.
+        if (without_morphology)
+            input = remove_morphology(input); // Deleting morphology nodes.
         if (postproc_id > 0 || mode <= 1)
             output_1 = JSON.stringify(input, undefined, mode == 0 ? 2 : 0);
         else output_1 = "";
         if (mode <= 1 || postproc_id == 1) {
             output_2 = "";
         } else {
-            input = new_postprocessor(input, ptproc !== null, with_spaces,
+            input = new_postprocessor(input, without_morphology, with_spaces,
                                       with_selmaho, !without_terminator);
             output_2 = JSON.stringify(input);
             output_2 = output_2.replace(/\"/gm, "");
@@ -179,6 +185,77 @@ function prefix_wordclass(tree, replacements) {
     return tree;
 }
 
+// ====== MORPHOLOGY REMOVAL ====== //
+
+/*
+ * remove_morphology(parse_tree)
+ * 
+ * This function takes a parse tree, and joins the expressions of the following
+ * nodes:
+ * "cmevla", "gismu_2", "lujvo", "fuhivla", "spaces"
+ * as well as any selmaho node (e.g. "KOhA").
+ * 
+ * (This is essentially a copy of process_parse_tree.js.)
+ */
+ 
+function remove_morphology(pt) {
+    if (pt.length < 1) return [];
+    var i;
+    /* Sometimes nodes have no label and have instead an array as their first
+       element. */
+    if (is_array(pt[0])) i = 0;
+    else { // The first element is a label (node name).
+        // Let's check if this node is a candidate for our pruning.
+        if (is_target_node(pt)) {
+            /* We join recursively all the terminal elements (letters) in this
+             * node and its child nodes, and put the resulting string in the #1
+             * slot of the array; afterwards we delete all the remaining elements
+             * (their terminal values have been concatenated into pt[1]). */
+            pt[1] = join_expr(pt);
+            // If pt[1] contains an empty string, let's delete it as well:
+            pt.splice((pt[1] == "") ? 1 : 2);
+            return pt;
+        }
+        i = 1;
+    }
+    /* If we've reached here, then this node is not a target for pruning, so let's
+       do recursion into its child nodes. */
+    while (i < pt.length) {
+        remove_morphology(pt[i]);
+        i++;
+    }
+    return pt;
+}
+
+/* Checks whether the argument node is a target for pruning. */
+function is_target_node(n) {
+    return (among(n[0], ["cmevla", "gismu", "lujvo", "fuhivla", "initial_spaces"])
+            || is_selmaho(n[0]));
+}
+
+/* This function returns the string resulting from the recursive concatenation of
+ * all the leaf elements of the parse tree argument (except node names). */
+function join_expr(n) {
+    if (n.length < 1) return "";
+    var s = "";
+    var i = is_array(n[0]) ? 0 : 1;
+    while (i < n.length) {
+        s += is_string(n[i]) ? n[i] : join_expr(n[i]);
+        i++;
+    }
+    return s;
+}
+
+function among(v, s) {
+    var i = 0;
+    while (i < s.length) if (s[i++] == v) return true;
+    return false;
+}
+
+function is_selmaho(v) {
+    if (!is_string(v)) return false;
+    return (0 == v.search(/^[IUBCDFGJKLMNPRSTVXZ]?([AEIOUY]|(AI|EI|OI|AU))(h([AEIOUY]|(AI|EI|OI|AU)))*$/g));
+}
 
 // ====== OLD POSTPROCESSOR ====== //
 
