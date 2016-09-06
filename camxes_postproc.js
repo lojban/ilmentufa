4eb32f1d61200f1806e3fbc1b85bcb2f6340d6d7
Ilmen <ilmen.pokebip@gmail.com>
2016-09-06 14:42:03 +0200

    camxes_postproc.js, camxes.html: Removing the old postprocessor. A few minor other changes.

--- camxes_postproc.js
+++ camxes_postproc.js
@@ -1,7 +1,7 @@
 /*
  * CAMXES.JS POSTPROCESSOR
  * Created by Ilmen (ilmen.pokebip <at> gmail.com) on 2013-08-16.
- * Last change: 2016-09-02.
+ * Last change: 2016-09-06.
  * 
  * Entry point: camxes_postprocessing(input, mode, ptproc, postproc_id)
  * Arguments:
@@ -19,11 +19,6 @@
  *         5 = Prettified - famyma'o
  *         6 = Prettified - famyma'o + selma'o
  *         7 = Prettified - famyma'o + selma'o + bridi parts
- *    -- ptproc:  OPTIONAL [function] parse tree processor
- *    -- postproc_id:  OPTIONAL [number] selecter postprocessors
- *         0 = New postprocessor
- *         1 = Old postprocessor
- *         2 = Both
  *         
  * Return value:
  *       [string] postprocessed version of camxes' output
@@ -40,16 +35,12 @@
  *   -- join_expr(n)
  *   -- among(v, s)
  *   -- is_selmaho(v)
- *   -- old_postprocessor(text, with_selmaho, without_terminator, with_nodes_labels)
- *   -- erase_elided_terminators(str)
- *   -- delete_superfluous_brackets(str)
  *   -- prettify_brackets(str)
  *   -- str_print_uint(val, charset)
  *   -- str_replace(str, pos, len, sub)
  *   -- chr_check(chr, list)
  *   -- is_string(v)
  *   -- is_array(v)
- *   -- dbg_bracket_count(str)
  */
 
 
@@ -64,50 +55,27 @@ if (typeof alert !== 'function')
  * If the new postprocessor proves satisfactory, the older postprocessor's code
  * will be entirely removed.
  */
-function camxes_postprocessing(input, mode, postproc_id) {
+function camxes_postprocessing(input, mode) {
+    if (!is_array(input))
+        return "Postprocessor error: invalid input type.";
     var with_spaces = mode & 8;
     var without_morphology = mode & 16;
     mode = mode % 8;
     var with_selmaho = (mode != 2 && mode != 5);
     var with_nodes_labels = (mode == 4 || mode == 7);
     var without_terminator = (mode >= 5);
-    var output_1, output_2;
-    // Default: older postprocessor.
-    if (typeof postproc_id !== 'number') postproc_id = 1;
-    if (is_array(input)) {
-        if (without_morphology)
-            input = remove_morphology(input); // Deleting morphology nodes.
-        if (postproc_id > 0 || mode <= 1)
-            output_1 = JSON.stringify(input, undefined, mode == 0 ? 2 : 0);
-        else output_1 = "";
-        if (mode <= 1 || postproc_id == 1) {
-            output_2 = "";
-        } else {
-            input = new_postprocessor(input, without_morphology, with_spaces,
-                                      with_selmaho, !without_terminator);
-            output_2 = JSON.stringify(input);
-            output_2 = output_2.replace(/\"/gm, "");
-            output_2 = output_2.replace(/,/gm, " ");
-        }
-    } else if (is_string(input)) {
-        if (input.charAt(0) != '[') return input;
-        if (postproc_id == 0)
-            return "/!\\ The new postprocessor doesn't support string input.";
-        output_1 = input;
-        output_2 = "";
-        if (mode >= 1) {
-            output_1 = output_1.replace(/ +/gm, "");
-            output_1 = output_1.replace(/[\r\n]/gm, "");
-        }
-    } else return "ERROR: Wrong input type.";
-    if (output_1 !== "" && mode >= 2)
-        output_1 = old_postprocessor(output_1, with_selmaho, without_terminator,
-                                     with_nodes_labels);
+    if (without_morphology)
+        input = remove_morphology(input); // Deleting morphology nodes.
     var output;
-    if (postproc_id == 2 && mode >= 2)
-        output = "\n\n=== New postprocessor ===\n\n" + output_2
-               + "\n\n=== Old postprocessor ===\n\n" + output_1;
-    else output = output_2 + output_1;
+    if (mode <= 1) {
+        output = JSON.stringify(input, undefined, mode == 0 ? 2 : 0);
+    } else {
+        input = new_postprocessor(input, without_morphology, with_spaces,
+                                  with_selmaho, !without_terminator);
+        output = JSON.stringify(input);
+        output = output.replace(/\"/gm, "");
+        output = output.replace(/,/gm, " ");
+    }
     // Replacing "spaces" with "_":
     output = output.replace(/([ \[\],])(initial_)?spaces(?=[ \[\],])/gm, "$1_");
     // Bracket prettification:
@@ -133,6 +101,7 @@ function new_postprocessor(input, no_morpho, with_spaces, with_selmaho, with_ter
                   : is_selmaho(v) && b && with_terminator); };
     }
     input = prune_unwanted_nodes(input, filter);
+    if (input === null) return [];
     if (with_selmaho && no_morpho) {
         var replacements = [["cmene", "C"], ["cmevla", "C"], ["gismu", "G"],
                             ["lujvo", "L"], ["fuhivla", "Z"]];
@@ -257,151 +226,6 @@ function is_selmaho(v) {
     return (0 == v.search(/^[IUBCDFGJKLMNPRSTVXZ]?([AEIOUY]|(AI|EI|OI|AU))(h([AEIOUY]|(AI|EI|OI|AU)))*$/g));
 }
 
-// ====== OLD POSTPROCESSOR ====== //
-
-function old_postprocessor(text, with_selmaho, without_terminator, with_nodes_labels) {
-	/** Prettified forms **/
-	text = text.replace(/\"/gm, ""); // Delete every quote marks
-	/* Save selmaho and brivla types */
-	text = text.replace(/(gismu|lujvo|fuhivla|cmene|cmevla),([A-Za-z']+)/g,
-                        "$1:$2");
-	text = text.replace(/([A-Za-z_]+),([A-Za-z']+)/g, "$1:$2");
-	/* Save a few nodes from deletion (optional) */
-	if (with_nodes_labels) {
-		text = text.replace(/prenex,/g, "PN");
-		text = text.replace(/sentence,\[\[terms,/g, "sentence,[BH[terms,");
-		text = text.replace(/bridi_tail_2,/g, "BT");
-	}
-	/* Delete every remaining node names and commas */
-	text = text.replace(/([A-Za-z0-9_]+,)+\[/g, "[");
-	text = text.replace(/,/gm, "");
-	/* If requested, erase every elided terminators from the output */
-	if (without_terminator)
-		text = erase_elided_terminators(text);
-	/* Cleanup */
-	text = delete_superfluous_brackets(text);
-	text = text.replace(/\[ +/g, "[");
-	text = text.replace(/ +\]/g, "]");
-	text = text.replace(/([A-Z] )\[/g, "$1[");
-	/* Abbreviations */
-	text = text.replace(/(cmene|cmevla):/g, "C:");
-	text = text.replace(/gismu:/g, "G:");
-	text = text.replace(/lujvo:/g, "L:");
-	text = text.replace(/fuhivla/g, "F");
-	text = text.replace(/BRIVLA/g, "Z");
-	/* Making things easier to read */
-	if (!with_selmaho) text = text.replace(/[A-Za-z_]+:/g, "");
-	text = text.replace(/\]\[/g, "] [");
-    // Removing stuff like "a:a" that may remain when morphology is kept:
-    text = text.replace(/[ \[\]][a-z]:([a-z'])[ \[\]]/gm, "$1");
-	text = text.replace(/ +/gm, " ");
-	text = text.replace(/^ +/, "");
-	return text;
-}
-
-function erase_elided_terminators(str) {
-	var i, j;
-	var parachute = 400;
-	str = str.replace(/([a-z']+)`/g, function (v) { return v.toUpperCase(); });
-	str = str.replace(/([A-Z]+)'([A-Z]+)`/g,"$1h$2`");
-	str = str.replace(/([A-Z]+)H([A-Z]+)`/g,"$1h$2`");
-	str = str.replace(/([A-Zh]+)`/g,"$1");
-	do {
-		i = str.search(/\[[A-Zh`]+\]/);
-		if (i < 0) break;
-		j = i + str.substr(i).indexOf("]");
-		while (i-- > 0 && ++j < str.length)
-			if (str[i] != '[' || str[j] != ']') break;
-		i++;
-		j--;
-		str = str_replace(str, i, j-i+1, "");
-	} while (parachute--);
-	return str;
-}
-
-function delete_superfluous_brackets(string) {
-	/* RATIONALE:
-	 * Here is done the dirty work of removing all those superfluous brackets
-	 * left after our earlier intense session of regexp replacements.
-	 * Admittedly not quite easy to read, but it does the job. :)
-	 */
-	str = string.split("");
-	var i = 0;
-	var parachute = 4000; // Against evil infinite loops.
-	if (str.length < 1) {
-		alert("ERROR: (str.length < 1) @delete_superfluous_brackets");
-		return str;
-	}
-	// We reach the first word
-	while (str[i] == "[") {
-		if (i >= str.length) return; // No word found.
-		i++;
-	}
-	/**
-		### FIRST CLEANUP: BRACKETS SURROUNDING SINGLE WORD ###
-	**/
-	do {
-		/** erase_surrounding_brackets **/
-		var j = i;
-		while (str[i].search(/[A-Za-z'_:~]/) == 0 && i < str.length) i++;
-		while (str[i] == ']') {
-			if (j <= 0) {
-				alert("ERROR: right bracket found without left counterpart.");
-				break;
-			}
-			j--;
-			while (str[j] == ' ') j--;
-			if (str[j] == '[') {
-				str[j] = ' ';
-				str[i++] = ' ';
-			} else break;
-		}
-		/** reach_next_word **/
-		while (str[i] == '[' || str[i] == ']' || str[i] == ' ' && i < str.length)
-			i++;
-	} while (i < str.length && --parachute > 0);
-	if (parachute <= 0)
-		alert("@delete_superfluous_brackets #1: INFINITE LOOP\ni = " + i
-		    + "\nstr from i:\n" + str.slice(i).join(""));
-	/**
-		### SECOND CLEANUP: BRACKETS SURROUNDING GROUPS OF WORDS ETC. ###
-	**/
-	i = 0;
-	parachute = 4000;
-	while (i < str.length && parachute-- > 0) {
-		var so, eo, j;
-		// FIRST STEP: reaching the next '['.
-		while (i < str.length && str[i] != '[') i++;
-		so = i;
-		while (i < str.length && str[i] == '[') i++;
-		eo = i;
-		if (i >= str.length) break;
-		if (i - so < 2) continue;
-		j = i;
-		i -= 2;
-		do {
-			var floor;
-			// Now we'll reach the ']' closing the aformentioned '['.
-			floor = 1;
-			while (j < str.length && floor > 0) {
-				if (str[j] == '[') floor++;
-				else if (str[j] == ']') floor--;
-				j++;
-			}
-			// We now erase superfluous brackets
-			while (str[j] == ']' && i >= so) {
-				str[i--] = ' ';
-				str[j++] = ' ';
-			}
-		} while (i >= so && j < str.length && parachute-- > 0);
-		i = eo;
-	}
-	if (parachute <= 0)
-		alert("@delete_superfluous_brackets #2: INFINITE LOOP\ni = " + i
-		    + "\nstr from i:\n" + str.slice(i).join(""));
-	return str.join("");
-}
-
 
 /* ================== */
 /* ===  Routines  === */
@@ -460,7 +284,7 @@ function chr_check(chr, list) {
 	if (!is_string(list)) return false;
 	do if (chr == list[i]) return true; while (i++ < list.length);
 	return false;
-}
+} // Currently unused.
 
 function is_string(v) {
     return $.type(v) === "string";
@@ -468,18 +292,6 @@ function is_string(v) {
 
 function is_array(v) {
     return $.type(v) === "array";
-}
-
-function dbg_bracket_count(str) {
-	var i = 0;
-	var x = 0;
-	var y = 0;
-	while (i < str.length) {
-		if (str[i] == '[') x++;
-		else if (str[i] == ']') y++;
-		i++;
-	}
-	alert("Bracket count: open = " + x + ", close = " + y);
 }
 
 if (typeof module !== 'undefined')
