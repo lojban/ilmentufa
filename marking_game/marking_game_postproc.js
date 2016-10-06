f5607d1a3fb04da736300b8a8637616e5335d524
Ilmen <ilmen.pokebip@gmail.com>
2016-10-06 18:07:39 +0200

    marking_game_postproc.js: Making is_string and is_array not dependent on jQuery; adding 'text' to the node whitelist; making marking_game_format() not add a space at the end of each node's string.

--- marking_game/marking_game_postproc.js
+++ marking_game/marking_game_postproc.js
@@ -4,7 +4,8 @@ function camxes_postprocessing(parse_tree) {
     /* Pruning morphology nodes. */
     parse_tree = remove_morphology(parse_tree);
     /* Removing every nodes except for those in the following whitelist. */
-    var wanted_nodes = ["dot_star", "sentence", "selbri", "sumti", "prenex"];
+    var wanted_nodes = ["text", "dot_star", "sentence", "selbri", "sumti", "prenex"];
+    // We included "text" so the outermost array also begins with a node name.
     parse_tree = simplify_parse_tree(parse_tree, wanted_nodes);
     /* Building and retreiving the marking game string. */
     return marking_game_format(parse_tree);
@@ -50,6 +51,8 @@ function marking_game_format(pt) {
     }
     if (b.length >= 2)
         s = b[0] + s + b[1];
+    if (s.length > 0 && s[s.length - 1] == ' ')
+        s = s.substring(0, s.length - 1);
     return s;
 }
 
@@ -190,11 +193,11 @@ function str_replace(str, pos, len, sub) {
 }
 
 function is_string(v) {
-    return $.type(v) === "string";
+    return Object.prototype.toString.call(v) === '[object String]';
 }
 
 function is_array(v) {
-    return $.type(v) === "array";
+    return Object.prototype.toString.call(v) === '[object Array]';
 }
 
 if (typeof module !== 'undefined')
