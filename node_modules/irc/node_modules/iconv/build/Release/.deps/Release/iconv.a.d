f98d852fd3c1e2919fc7ba42d09e9ee6e19a490c
Ilmen-vodhr <ilmen.pokebip@gmail.com>
2019-06-06 00:02:39 +0200

    Ceasing to track node_modules; use package.json to fetch the required dependencies.

--- node_modules/irc/node_modules/iconv/build/Release/.deps/Release/iconv.a.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/iconv.a := rm -rf "Release/iconv.a" && cp -af "Release/obj.target/iconv.a" "Release/iconv.a"
--- node_modules/irc/node_modules/iconv/build/Release/.deps/Release/obj.target/iconv.a.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/obj.target/iconv.a := rm -f Release/obj.target/iconv.a && ar crs Release/obj.target/iconv.a Release/obj.target/libiconv/deps/libiconv/lib/iconv.o
